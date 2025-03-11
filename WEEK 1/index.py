1a48f6a7db4da3208414a337ece71c33cdc2330d
berena-code <jeanguya38@gmail.com>
2025-03-11 11:12:37 +0300

    Daye 1

---
+++ WEEK 1/index.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+
+number = (1, 2, 3, 4, 5)
+
+print (number) 
+
+echo "# Python-Journey" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/berena-code/Python-Journey.git
+git push -u origin main
